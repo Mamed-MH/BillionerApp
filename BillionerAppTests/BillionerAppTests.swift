588e4e9a81d3ac09e853ec655090e634b8843ce2
Mamed-MH <muhammedmz0619@gmail.com>
2024-10-07 19:17:03 +0400

    Initial Commit

---
+++ BillionerAppTests/BillionerAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  BillionerAppTests.swift
+//  BillionerAppTests
+//
+//  Created by Mamed Hacıyev on 7.10.2024.
+//
+
+import Testing
+@testable import BillionerApp
+
+struct BillionerAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
